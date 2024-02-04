61cc833b5a5f9d0a194bf09dacb7279ac595c60d
Ssemakula Mukiibi <Ssemakula@users.noreply.github.com>
2024-02-04 17:59:29 +0300

    Add project files.

---
+++ LogicMethods/LogicMethods.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace LogicMethods
+{
+    public static class LogicMethods
+    {
+        /* Doing C type ifs on numbers
+                * Will create a public class
+                */
+        public static bool IsTrue(byte a)
+        {
+            return a != 0;
+        }
+
+        public static bool IsTrue(sbyte a)
+        {
+            return a != 0;
+        }
+
+        public static bool IsTrue(char a)
+        {
+            return a != 0;
+        }
+
+        public static bool IsTrue(short a)
+        {
+            return a != 0;
+        }
+
+        public static bool IsTrue(ushort a)
+        {
+            return a != 0;
+        }
+
+
+        public static bool IsTrue(int a)
+        {
+            return a != 0;
+        }
+
+        public static bool IsTrue(uint a)
+        {
+            return a != 0;
+        }
+
+        public static bool IsTrue(long a)
+        {
+            return a != 0;
+        }
+
+        public static bool IsTrue(ulong a)
+        {
+            return a != 0;
+        }
+
+        public static bool IsTrue(float a)
+        {
+            return a != 0;
+        }
+
+        public static bool IsTrue(decimal a)
+        {
+            return a != 0;
+        }
+
+        public static bool IsTrue(bool a)
+        {
+            return a;
+        }
+
+        public static bool IsTrue(string a)
+        {
+            return !String.IsNullOrEmpty(a);
+        }
+    }
+}
+
